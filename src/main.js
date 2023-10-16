24bd1506e4dcf3a6558214d319a6adf7a28a7cc7
localjohny <zahar4747@gmail.com>
2023-10-16 15:24:02 +0600

    add google analystics

--- src/main.js
+++ src/main.js
@@ -11,12 +11,23 @@ import { useDataStore } from './stores/data'
 import App from './App.vue'
 import router from './router'
 
+import VueGtag from 'vue-gtag'
+
 const pinia = createPinia()
 const app = createApp(App)
 
 app.use(router)
 app.use(pinia)
 app.use(BootstrapVueNext)
+app.use(
+  VueGtag,
+  {
+    appName: import.meta.env.VITE_MEASUREMENT_NAME,
+    pageTrackerScreenviewEnabled: true,
+    config: { id: import.meta.env.VITE_MEASUREMENT_ID }
+  },
+  router
+)
 
 const store = useDataStore()
 initialize(store, router)
